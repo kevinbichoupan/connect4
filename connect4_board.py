45346b8cc2ec09af4915934be5e9c5a70bdc6dcf
Kevin Bichoupan <kevinbichoupan@Kevins-Air-2.lan>
2024-12-09 20:49:17 -0500

    connect 4 board object created

--- connect4_board.py
+++ connect4_board.py
@@ -1,15 +1,15 @@
 
-
-
 class Connect4():
     def __init__(self):
         self.position = []
         self.rows = 6
         self.columns = 7
+        self.has_winner = False
+        self.winner = None
         
     def check_for_available_slots(self):
         available_slots = []
-        for i in range(1,7):
+        for i in range(1,8):
             if self.position.count(i) < 6:
                 available_slots.append(i)
         return available_slots
@@ -24,26 +24,51 @@ class Connect4():
         # create list of sequences that show the rows, columns, and diagnals of the board
         # search each of the list of sequences for 4 consecutive markers 
         
-        row_check = [[],[],[],[],[],[]]
-        column_check = [[],[],[],[],[],[],[]]
-        #diagonal check
+        row_check = ['', '', '', '', '', '']
+        column_check = ['', '', '', '', '', '', '']
 
         for i in range(0,len(self.position)):
             marker = 'A' if i % 2 == 0 else 'B'
-            column_check[self.position[i]].append(marker)
+            column_check[self.position[i] - 1] += marker
         
+        for i in range(0,7):
+            column_check[i] = column_check[i].ljust(6,'O')
+
         for column in column_check:
             for row in range(0,len(column)):
-                row_check[row].append(column[row])
+                row_check[row] += column[row]
 
-        print(row_check)
         print(column_check)
+        print(row_check)
+
+        for i in row_check:
+            if 'AAAA' in i:
+                self.has_winner = True
+                return 'A'
+            if 'BBBB' in i:
+                self.has_winner = True
+                return 'B'
+
+        for i in column_check:
+            if 'AAAA' in i:
+                self.has_winner = True
+                return 'A'
+            if 'BBBB' in i:
+                self.has_winner = True
+                return 'B'
+
+        for i in range(0,4):
+            for j in range(0,3):
+                try:
+                    if column_check[i][j] == column_check[i+1][j+1] == column_check[i+2][j+2] == column_check[i+3][j+3] != 'O':
+                        self.has_winner = True
+                        return column_check[i][j]
+                    if column_check[i][6-j] == column_check[i+1][6-(j+1)] == column_check[i+2][6-(j+2)] == column_check[i+3][6-(j+3)] != 'O':
+                        self.has_winner = True
+                        return column_check[i][6-j]
+                except:
+                    pass
 
     def execute_turn(self, position):
         self.place_marker(self, position)
         self.check_for_winner()
-
-a = Connect4()
-for i in [1, 2, 3, 4, 1, 2, 3, 4]:
-    a.place_marker(i)
-a.check_for_winner()
